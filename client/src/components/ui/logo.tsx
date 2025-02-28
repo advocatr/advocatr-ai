71168d6ce774c957512829e4f8d8e4220bb0d7d7
Jack Booth <jack_booth@mac.com>
2025-02-28 10:34:59 +0000

    Logo Updates

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -15,7 +15,7 @@ export function Logo({ className, size = "md" }: LogoProps) {
 
   return (
     <img
-      src="client/src/components/ui/advocatr-logo 500px.png"
+      src="/client/ssrc/components/ui/advocatr-logo-500px.png"
       alt="Advocatr Logo"
       className={cn(sizeClasses[size], className)}
     />
