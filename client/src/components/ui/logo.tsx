bc7c01067bfa47ef7eb4d78e182e83c696e7c9cf
Jack Booth <jack_booth@mac.com>
2025-02-28 10:04:10 +0000

    Logo amendments

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { cn } from "@/lib/utils";
 
@@ -11,13 +10,13 @@ export function Logo({ className, size = "md" }: LogoProps) {
   const sizeClasses = {
     sm: "h-6 w-6",
     md: "h-8 w-8",
-    lg: "h-10 w-10"
+    lg: "h-10 w-10",
   };
 
   return (
-    <img 
-      src="/advocatr-logo-500px.png" 
-      alt="Advocatr Logo" 
+    <img
+      src="client/src/components/ui/advocatr-logo 500px.png"
+      alt="Advocatr Logo"
       className={cn(sizeClasses[size], className)}
     />
   );
